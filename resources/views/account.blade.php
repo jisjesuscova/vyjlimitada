1d4130f2742447c7f6a30f02f6b09d80f2343cd2
jisjesuscova <jesuscova@jisparking.com>
2023-07-20 07:39:04 -0400

    cambios

--- resources/views/account.blade.php
+++ resources/views/account.blade.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>Ingebsoluciones - Soluciones Integrales</title>
+  <title>V y J Limitada</title>
 
   <!-- Google Font: Source Sans Pro -->
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
